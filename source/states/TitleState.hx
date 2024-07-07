4d61fcc5de1292d9a46da24f99f3d24081fddb3d
Natunatic <animaltoweroficial@gmail.com>
2024-07-07 19:02:58 -0400

    nosexd

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -245,8 +245,7 @@ class TitleState extends MusicBeatState
 			//EDIT THIS ONE IF YOU'RE MAKING A SOURCE CODE MOD!!!!
 			//EDIT THIS ONE IF YOU'RE MAKING A SOURCE CODE MOD!!!!
 				gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
-				gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-				gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+				gfDance.animation.addByPrefix('danceLeft', 'God Eater Spin', 24, false);
 		}
 
 		add(gfDance);
