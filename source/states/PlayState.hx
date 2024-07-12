5691f7e610c60e12b5d10c9f7d0114e768e1a5c9
Natunatic <animaltoweroficial@gmail.com>
2024-07-11 20:43:48 -0400

    new file:   assets/shared/images/crazystage.png 	modified:   source/states/PlayState.hx 	new file:   source/states/stages/CrazyStage.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -421,6 +421,7 @@ class PlayState extends MusicBeatState
 			case 'school': new states.stages.School(); //Week 6 - Senpai, Roses
 			case 'schoolEvil': new states.stages.SchoolEvil(); //Week 6 - Thorns
 			case 'tank': new states.stages.Tank(); //Week 7 - Ugh, Guns, Stress
+			case 'crazyStage': new states.stages.CrazyStage(); // Demo - CrAzY StAgE
 		}
 
 		if(isPixelStage) {
